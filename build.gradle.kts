57edd0d67e694fb6972df587b5c1e6f9091cee17
kamilkalisz <kamil.kalisz@gmail.com>
2025-02-15 19:54:54 +0100

    added detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 plugins {
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply  false
+    alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
+    alias(libs.plugins.detekt) apply true
+}
+
+detekt {
+    toolVersion = "1.23.7"
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    parallel = true
+    buildUponDefaultConfig = true
+    allRules = false
+    autoCorrect = true
 }
